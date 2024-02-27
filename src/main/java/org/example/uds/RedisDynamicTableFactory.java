0d85d67a33ca1039722b5d0a2990b441a22ac3af
congdai-airwallex <cong.dai@airwallex.com>
2024-02-27 16:42:59 +0800

    add user defined source connector

---
+++ src/main/java/org/example/uds/RedisDynamicTableFactory.java
@@ -0,0 +1,60 @@
+package org.example.uds;
+
+import org.apache.flink.configuration.ConfigOption;
+import org.apache.flink.configuration.ConfigOptions;
+import org.apache.flink.configuration.ReadableConfig;
+import org.apache.flink.table.factories.DynamicTableSourceFactory;
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.factories.FactoryUtil;
+import org.apache.flink.table.types.DataType;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class RedisDynamicTableFactory implements DynamicTableSourceFactory {
+    // define all options statically
+    public static final ConfigOption<String> HOSTNAME = ConfigOptions.key("host")
+            .stringType()
+            .noDefaultValue();
+    public static final ConfigOption<Integer> PORT = ConfigOptions.key("port")
+            .intType()
+            .noDefaultValue();
+    public static final ConfigOption<String> PASSWORD = ConfigOptions.key("password")
+            .stringType()
+            .noDefaultValue();
+    @Override
+    public String factoryIdentifier() {
+        return "redis"; // used for matching to `connector = '...'`
+    }
+    @Override
+    public Set<ConfigOption<?>> requiredOptions() {
+        final Set<ConfigOption<?>> options = new HashSet<>();
+        options.add(HOSTNAME);
+        options.add(PORT);
+        options.add(PASSWORD);
+        return options;
+    }
+
+    @Override
+    public Set<ConfigOption<?>> optionalOptions() {
+        final Set<ConfigOption<?>> options = new HashSet<>();
+        return options;
+    }
+
+    @Override
+    public DynamicTableSource createDynamicTableSource(Context context) {
+        final FactoryUtil.TableFactoryHelper helper = FactoryUtil.createTableFactoryHelper(this, context);
+        // validate all options
+        helper.validate();
+        // get the validated options
+        final ReadableConfig options = helper.getOptions();
+        final String hostname = options.get(HOSTNAME);
+        final int port = options.get(PORT);
+        final String password = options.get(PASSWORD);
+        // derive the produced data type (excluding computed columns) from the catalog table
+        final DataType producedDataType =
+                context.getCatalogTable().getResolvedSchema().toPhysicalRowDataType();
+
+        return new RedisDynamicTableSource(hostname, port, password, producedDataType);
+    }
+}
---
+++ src/main/java/org/example/uds/RedisDynamicTableSource.java
@@ -0,0 +1,34 @@
+package org.example.uds;
+
+import org.apache.flink.table.connector.source.DynamicTableSource;
+import org.apache.flink.table.connector.source.LookupTableSource;
+import org.apache.flink.table.connector.source.TableFunctionProvider;
+import org.apache.flink.table.types.DataType;
+
+public class RedisDynamicTableSource implements LookupTableSource {
+    private final String hostname;
+    private final int port;
+    private final String password;
+    private final DataType producedDataType;
+    public RedisDynamicTableSource(String hostname, int port, String password, DataType producedDataType) {
+        this.hostname = hostname;
+        this.port = port;
+        this.password = password;
+        this.producedDataType = producedDataType;
+    }
+
+    @Override
+    public LookupRuntimeProvider getLookupRuntimeProvider(LookupContext context) {
+        return TableFunctionProvider.of(new RedisLookUpFunction(hostname, port, password));
+    }
+
+    @Override
+    public DynamicTableSource copy() {
+        return new RedisDynamicTableSource(hostname, port, password, producedDataType);
+    }
+
+    @Override
+    public String asSummaryString() {
+        return "Redis Table Source";
+    }
+}
