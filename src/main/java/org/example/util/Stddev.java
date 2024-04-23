2b7f1ac1ef516707d7802bf6a227a1f99ad58b38
congdai-airwallex <cong.dai@airwallex.com>
2024-04-23 18:10:47 +0800

    fix bugs

--- src/main/java/org/example/util/Stddev.java
+++ src/main/java/org/example/util/Stddev.java
@@ -5,20 +5,18 @@ import org.apache.flink.table.functions.AggregateFunction;
 import java.util.List;
 
 public class Stddev extends AggregateFunction<Double, StddevAccumulator> {
-
     @Override
     public Double getValue(StddevAccumulator acc) {
-        List<Double> list = acc.nums.getList();
-        if(list.size() == 0 || list.size() == 1) {
+        if(acc.count == 0 || acc.count == 1) {
             return null;
-        } else {
-            Double avg = list.stream().mapToDouble(a -> a).average().orElse(0.0);
-            Double sum = 0.0;
-            for(Double num : list) {
-                sum += (num-avg)*(num-avg);
-            }
-            return Math.sqrt(sum/(list.size()-1));
         }
+        List<Double> list = acc.nums.getList();
+        double avg = acc.sum / acc.count;
+        double squaredDiffSum = 0.0;
+        for(double num : list) {
+            squaredDiffSum  += Math.pow(num - avg, 2);
+        }
+        return Math.sqrt(squaredDiffSum/(acc.count-1));
     }
 
     @Override
@@ -27,14 +25,22 @@ public class Stddev extends AggregateFunction<Double, StddevAccumulator> {
     }
 
     public void accumulate(StddevAccumulator acc, Double value) throws Exception {
+        if(value == null) return;
+        acc.count++;
+        acc.sum += value;
         acc.nums.add(value);
     }
 
     public void retract(StddevAccumulator acc, Double value) throws Exception {
+        if(value == null) return;
+        acc.count--;
+        acc.sum -= value;
         acc.nums.remove(value);
     }
 
     public void resetAccumulator(StddevAccumulator acc) {
+        acc.count = 0;
+        acc.sum = 0.0;
         acc.nums.clear();
     }
 }
--- src/main/java/org/example/util/StddevAccumulator.java
+++ src/main/java/org/example/util/StddevAccumulator.java
@@ -2,9 +2,8 @@ package org.example.util;
 
 import org.apache.flink.table.api.dataview.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class StddevAccumulator {
     public ListView<Double> nums = new ListView<>();
+    public int count = 0;
+    public double sum = 0.0;
 }
